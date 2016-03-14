0aab666d28211b50ab6cbd06f1c59cbc150abf48
Silver-Shen <shenzy13@qq.com>
2016-03-14 13:23:23 +0800

    lab2-spoc fix

--- lab2-spoc/translate.c
+++ lab2-spoc/translate.c
@@ -3,6 +3,7 @@
 int main(){
     int va, pa, i;
     FILE *fp = fopen("input.txt","r");
+    FILE *fout = fopen("output.txt","w");
     for (i=0; i<10; i++){
         fscanf(fp,"va %x, pa %x\n", &va, &pa);
         int pde_idx, pde_ctx, pte_idx, pte_ctx;
@@ -10,7 +11,7 @@ int main(){
         pde_ctx = (pde_idx - 0x300 + 1) << 12 | 0x3;
         pte_idx = (va >> 12) & 0x3FF;
         pte_ctx = pa & ~0x3FF | 0x3;
-        printf("va %08x, pa %08x, pde_idx %08x, pde_ctx %08x, pte_idx %08x, pte_ctx %08x\n", 
+        fprintf(fout, "va %08x, pa %08x, pde_idx %08x, pde_ctx %08x, pte_idx %08x, pte_ctx %08x\n", 
             va, pa, pde_idx, pde_ctx, pte_idx, pte_ctx);
     }
     return 0;
