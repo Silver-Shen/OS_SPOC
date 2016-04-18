6f3fafa9a737019a26b209f76e21a7f57a89cf9f
qq775193759 <775193759@qq.com>
2016-04-18 10:58:19 +0800

    add output

--- RR/rr.py
+++ RR/rr.py
@@ -16,15 +16,20 @@ def input_porc():
 def proc_run(proc,time):
     co = 0
     proc_num = len(proc)
+    total = 0
     while(proc_num > 0):
         for i in range(0,len(proc)):
             if(proc[i] > time):
+                total = total + time
                 proc[i] = proc[i] - time
-                print proc[i]
+                print "[ time   %-2d ] Run job   %d for %.2f secs"%(co,i,time)
+                co  = co + 1
             elif(proc[i] > 0):
+                total = total + proc[i]
+                print "[ time   %-2d ] Run job   %d for %.2f secs ( DONE at %.2f )"%(co,i,time,total)
                 proc[i] = 0
                 proc_num = proc_num - 1
-                print proc[i]
+                co  = co + 1
 
 
 if __name__ == "__main__":
